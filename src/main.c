edefd59dcc082ba45d6e0a6cc8604b7a702a0fdb
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-08-10 15:33:58 +0000

    Added Commandline Options (getopt)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <alpm.h>
+#include <unistd.h>
+#include <getopt.h>
+
+void list_all(void);
+
+struct option args[] = {
+    {"listall", no_argument, NULL, 'l'},
+};
 
 static alpm_handle_t *handle;
 
-int main(void)
+int main(int argc, char **argv)
+{
+    int c =0;
+    while(( c = getopt_long(argc, argv, ":l", args, NULL)) != -1)
+    {
+        switch(c)
+        {
+            case 'l':
+                list_all();
+            break;
+        }
+    }
+    return 0;
+}
+
+void list_all(void)
 {
     handle = alpm_initialize("/","/var/lib/pacman",NULL);
     alpm_list_t *sync_dbs = alpm_get_syncdbs(handle);
@@ -24,10 +47,8 @@ int main(void)
         alpm_list_t *i, *cache = alpm_db_get_pkgcache(db);
         for(i = cache; i; i = alpm_list_next(i))
         {
-            printf("PKG: %s\n", alpm_pkg_get_name(i->data));
+            printf("PKG: %s (%s)\n", alpm_pkg_get_name(i->data), alpm_pkg_get_version(i->data));
         }
 
     }
-
-    return 0;
 }
